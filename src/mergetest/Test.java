32e1a1eec5f8bfb93b83f4620128d6f4addc360f
yangfa4j <849949161@qq.com>
2019-11-20 16:27:53 +0800

    merge Test

---
+++ src/mergetest/Test.java
@@ -0,0 +1,12 @@
+package mergetest;
+
+/**
+ * @Date 2019/11/20
+ * @Author yangfa
+ * @Description
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("我是在Hot_fix上写的代码");
+    }
+}
